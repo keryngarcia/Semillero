155a6ec409a0fa2cb0fc06f3074be19790043eb2
keryngarcia <keryngarcia@hotmail.es>
2019-12-04 13:20:39 -0500

    componente persona

---
+++ semillero2019/ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/persona/persona.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+/**
+ * @description Componente persona en donde se establecen el nombre, apellido y ciudad
+ * 
+ * @author Keryn García Espitaleta <keryngarcia@hotmail.es>
+ */
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.css']
+})
+export class PersonaComponent implements OnInit {
+
+  // declaracon de variables
+  nombre: string ;
+  ciudad: string ;
+  apellido: string;
+
+  /**
+   * constructor 
+   */
+  constructor() { 
+    this.nombre='Keryn';
+    this.apellido = 'García';
+    this.ciudad = 'Cereté'
+  }
+
+  getNombre(): string{
+    return this.nombre
+  }
+
+  setNombre(nombre: string):void{
+    this.nombre = nombre;
+  }
+
+  getApellido():string{
+    return this.apellido;
+  }
+
+  setApellido(apellido: string):void{
+    this.apellido = apellido;
+  }
+
+  getCiudad(){
+    return this.ciudad;
+  }
+
+  setCiudad(ciudad: string):void{
+    this.ciudad = ciudad;
+  }
+
+  
+  ngOnInit() {
+  }
+
+}
